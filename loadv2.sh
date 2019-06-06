72687c70e6a6d56b2d7ddc1db5adea9f22e90324
aosterloh <aosterloh@google.com>
2019-06-06 08:17:17 +0200

    Update loadv2.sh

--- loadv2.sh
+++ loadv2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# reads list of parquet files off GCS and imports into BigQuery
-# csv has 3 columns: datasetName,tableName,GCS_URI
-project="replace with your projectID"
+#expecting comma separated file with 3 columns:
+#dataset_name,table_name,GCS_URI
+
+project="data-academy-2018"
 region="EU"
 replace="false" #true overwrites existing table, false is write_append
 
 tables=0
 datasets=0
-
+start=`date +%s`
+NOW=$(date "+%Y.%m.%d-%H.%M.%S")
+logfile="log-$NOW.log"
 
 # takes 1 argument, the csv file name, e.g. loadv2.sh input.csv
 if [ "$#" -ne 1 ]; then
@@ -16,31 +19,40 @@ if [ "$#" -ne 1 ]; then
 fi
 input=$1
 
-# add new line after last entry in csv or adapt while loop
-# see https://unix.stackexchange.com/questions/321056/while-read-loop-stops-after-the-first-line-even-though-it-doesnt-read-from-stdi
+echo "Starting new import at $date" > $logfile
+
 while IFS=',' read -r dname tname uri
 do
-    gcsexist=$(gsutil stat $uri) #check gcs uri exists
+  gcsexist=$(gsutil stat $uri)
     if [[ $gcsexist == *"Creation time"* ]]; then
       #create dataset if not exists
       exists=$(bq ls -d | grep -w $dname)
       if [ -n "$exists" ]; then
-         echo "Not creating dataset $dname since it already exists"
+         echo "Not creating dataset $dname since it already exists\n" >> $logfile
       else
-         echo "Creating dataset $dname"
-         bq --project_id="$project" --location="$region" mk "$dname"
+         echo "Creating dataset $dname\n" >> import.log
+         bq --project_id="$project" --location="$region" mk "$dname" >> $logfile
          ((datasets+=1))
       fi
 
-      bq --project_id="$project"  --location="$region" load --replace="$replace" --source_format=PARQUET "$dname"."$tname" "$uri"
+      bq --project_id="$project"  --location="$region" load --replace="$replace" --source_format=PARQUET "$dname"."$tname" "$uri" >> $logfile
       ((tables+=1))
   else 
-      echo "Skipping import of table $tname as URI $uri cannot be found"
+      echo "Skipping import of table $tname as URI $uri cannot be found\n" >> $logfile
   fi
       
 done < "$input"
 
-echo "Done importing data for project $project in region $region"
+end=`date +%s`
+
+echo 
+echo "Import took $((end-start)) seconds" >> $logfile
+echo "Done importing data for project $project in region $region" >> $logfile
+echo "Using CSV $input" >> $logfile
+echo "Created $datasets new datasets" >> $logfile
+echo "Created $tables tables using replace = $replace" >> $logfile
+echo "Import took $((end-start)) seconds" 
+echo "Done importing data for project $project in region $region" 
 echo "Using CSV $input"
-echo "Created $datasets new datasets"
-echo "Created $tables tables using replace = $replace"
+echo "Created $datasets new datasets" 
+echo "Created $tables tables using replace = $replace" 
