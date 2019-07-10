54cf6818089a9ad251826ccc090d2481ae646f3d
aosterloh <aosterloh@google.com>
2019-07-10 11:25:26 +0200

    Update verify_missing_files.sh

--- verify_missing_files.sh
+++ verify_missing_files.sh
@@ -9,16 +9,12 @@ fi
 
 while IFS=',' read -r  dname tname uri
 do
-    echo "Checking $uri "
+    #echo "Checking $uri "
     gcsexist=$(gsutil -q stat $uri ; echo $? )
     if [ $gcsexist -eq 0 ]; then
       #load the data
-      echo "$uri EXISTS EXISTS EXISTS EXISTS EXISTS" 
+      echo "$uri EXISTS" 
       echo " "
-    else 
-	    # uri not valid, hence skip and log it  
-	    echo "$uri does not exist" 
-	    echo " "
     fi
 done < $input
 
