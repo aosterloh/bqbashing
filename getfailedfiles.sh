48e218709f0064fb916562cdfab665eb9752f829
aosterloh <aosterloh@google.com>
2019-07-14 13:08:09 +0200

    Add files via upload

---
+++ getfailedfiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#change this one
+input_csv=csv-jul3-clean.csv 
+
+# Don't change anything else
+input=$1
+tempfile=failed_jobs.log
+broken_parquet=broken_parquet.csv
+jobids=jobids.log
+
+rm $tempfile
+rm $broken_parquet
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Wrong num of arguments. Call script along with timestamp in ms (only consider jobs after that time)"
+    exit 1
+fi
+
+bq ls -j -a -n 1200 --min_creation_time $1 | grep FAILURE >> $tempfile 
+
+
+while IFS=' ' read -r  id t2 t3 t4 t5 t6 t7 t8 t9 t10
+do
+    
+    output=$(bq show -j $id | grep ".parquet" | cut -d' ' -f 3 |  tr -d :)
+    cat $input_csv | grep $output | tee -a $broken_parquet 
+    
+
+done < $tempfile
+
