c78a9313bf924b0f2df8e1e9f967aa130e850a29
aosterloh <aosterloh@google.com>
2019-08-05 18:10:56 +0200

    Update ParquetFileCleanerv4.py

--- ParquetFileCleanerv4.py
+++ ParquetFileCleanerv4.py
@@ -1,31 +1,21 @@
-
-
-
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 import pandas as pd
 import datetime
+import string
 from pyspark.sql import functions as F 
 
 #sc = SparkContext('local')
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 
+#get this csv using getfailedfiles.sh
+csv_parquet = "gs://kf-eu/broken/broken5.csv"
 
-# In[ ]:
-
-
-# read csv with parquet files that bq load will not process 
-csv_parquet = "gs://kf-eu/broken/broken_parquet2.csv"
-fixed_bucket = "gs://kf-eu/fixed/"
-
-
-csv = pd.read_csv(csv_parquet, usecols=[2])
-
-
-for index, row in csv.iterrows():
-    # access data using column names
+for row in spark.read.option("header","true").csv(csv_parquet).collect():
     url = row['url']
+
+    output_file = string.replace(url,'Blobstore','Blobstore2')
     file_name = url.rsplit('/', 1)[-1]
     print ("Transforming " + file_name)
     df = spark.read.parquet(url) # load parquet file into a spark dataframe
@@ -41,7 +31,6 @@ for index, row in csv.iterrows():
         max_allowed_date = datetime.date(datetime.MAXYEAR - 1, 1, 1) 
         df_fixed = df.withColumn('dat_bis', F.when(F.col('dat_bis') >= max_date, max_allowed_date).otherwise(F.col('dat_bis')))
         df_repartitioned = df_fixed.repartition(1)
-        output_file=fixed_bucket+file_name
         print "Writing to "
         print output_file
         file = df_repartitioned.coalesce(1).write.format("parquet").save(output_file)
@@ -52,5 +41,3 @@ for index, row in csv.iterrows():
     else:
         print ("Skipping transformation for " + file_name + " because dat_bis does not exist")
     df.unpersist()
-    
-
